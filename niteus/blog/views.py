91374f204c724093b0c0427d3a7d37bf8c8151e2
Alexey Sergeev <niteusmail@gmail.com>
2024-06-23 17:10:54 +0300

    Add detail page

--- niteus/blog/views.py
+++ niteus/blog/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render
 from .models import Menu
 from .models import Article
+from .models import Article, Menu
+from django.shortcuts import render, get_object_or_404
 
 def menu_view(request):
     menus = Menu.objects.all()
@@ -11,3 +13,7 @@ def home(request):
     articles = Article.objects.all()
     menus = Menu.objects.prefetch_related('items').all()
     return render(request, 'blog/home.html', {'articles': articles, 'menus': menus})
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    return render(request, 'blog/article_detail.html', {'article': article})
--- niteus/niteus/urls.py
+++ niteus/niteus/urls.py
@@ -4,17 +4,16 @@ from django.urls import path
 from blog.views import home
 from django.conf import settings
 from django.conf.urls.static import static
+from blog.views import home, article_detail
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-
+    path('article/<int:article_id>/', article_detail, name='article_detail'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
